63e294048195c019c2d16d02ca0c4bdabb228582
yashweybee <yashparmar@weybee.in>
2024-01-25 19:43:55 +0530

    -notification added

--- src/Components/SearchTodo.jsx
+++ src/Components/SearchTodo.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchTodo = () => {
-  return <div>SearchTodo</div>;
+  const [searchText, setSearchText] = useState("");
+  const handeSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handeSubmit}>
+        <input type="text" onChange={(e) => setSearchText(e.target.value)} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchTodo;
